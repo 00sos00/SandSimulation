56c5bdbf7386661dadb5afbc8e9c8bff3ebf9a2e
sxsb <42996937+00sos00@users.noreply.github.com>
2023-09-12 06:38:28 +0300

    Added needed libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
 
 int main() {
-	std::cout << "Sand Sim" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Sand Simulation", NULL, NULL);
+
+	if (!window) {
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// TODO: Initialize other stuff...
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 }
